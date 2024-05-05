54c43ec568a390a217c3f12dd821cff3dbbd45e8
Leonodes <20102514d@connect.polyu.hk>
2024-05-05 09:05:20 +0800

    Update video2png.py

--- video_utils/video2png.py
+++ video_utils/video2png.py
@@ -1,40 +1,42 @@
-import cv2
+import imageio
 import os
+from PIL import Image
 
-def extract_frames(video_path, output_folder):
+def extract_frames(video_path, output_folder, frame_size=(128, 128)):
+    # Check if the video path exists
+    if not os.path.exists(video_path):
+        print("Error: Video path does not exist.")
+        return
+
     # Create output folder if it doesn't exist
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
-    # Open the video file
-    cap = cv2.VideoCapture(video_path)
-    
-    # Initialize variables
-    frame_count = 1
-    size = (128, 128)
+    # Use imageio to read the video
+    reader = imageio.get_reader(video_path)
+    frame_count = 0
 
-    # Read until the end of the video
-    while True:
-        # Read a frame from the video
-        ret, frame = cap.read()
-        frame_resized = cv2.resize(frame, size)
-        
-        # Break the loop if no frame is retrieved
-        if not ret:
-            break
-        
-        # Save the frame as a PNG file
-        frame_path = os.path.join(output_folder, f"frame_{frame_count:05d}.png")
-        cv2.imwrite(frame_path, frame_resized)
+    try:
+        for i, frame in enumerate(reader):
+            # Convert array to an image
+            image = Image.fromarray(frame)
+            # Resize the image
+            image = image.resize(frame_size)
 
-        # Increment frame count
-        frame_count += 1
+            # Save the frame as a PNG file
+            frame_path = os.path.join(output_folder, f"frame_{frame_count:05d}.png")
+            image.save(frame_path)
 
-    # Release the video capture object
-    cap.release()
+            # Increment frame count
+            frame_count += 1
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        reader.close()
+
     print(f"Frames extracted: {frame_count}")
 
 # Example usage
-video_path = "C:\\xampp\\htdocs\\video_compressor\\videos\\orig_kobe.mp4"
-output_folder = "frames"
+video_path = "C:\\Users\\Leon\\Desktop\\video_compressor_gray\\videos\\cm_kobe.mp4"  # Adjust path as necessary
+output_folder = "C:\\Users\\Leon\\Desktop\\video_compressor_gray\\frames\\cm_frames"
 extract_frames(video_path, output_folder)
